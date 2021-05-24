5819a2b7abba64a8fa22392ca4dd84df76f5e643
Amy He <39035908+Amyh11325@users.noreply.github.com>
2021-05-23 18:33:30 -0700

    back button and comments

--- lib/home.dart
+++ lib/home.dart
@@ -12,25 +12,46 @@ class Home extends StatefulWidget {
 
 //https://willowtreeapps.com/ideas/how-to-use-flutter-to-build-an-app-with-bottom-navigation
 //https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html
+//https://medium.com/@swav.kulinski/flutter-navigating-off-the-charts-e118562a36a5
+//https://medium.com/flutter/getting-to-the-bottom-of-navigation-in-flutter-b3e440b9386
 /// This is the private State class that goes with MyStatefulWidget.
+
 class _HomeState extends State<Home> {
   //for navigation
-  int _currentIndex = 1;
-  final List<Widget> _children = [
-    SocialPage(),
-    CalendarPage(),
-    ProfilePage(),
-  ];
+  static final navigatorKey = GlobalKey<NavigatorState>();
+  final _pages = {
+    "/": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text("HomePage"),
+          ),
+        ),
+    "SocialPage": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text("HomePage"),
+          ),
+        ),
+    "CalendarPage": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text("HomePage"),
+          ),
+        ),
+    "ProfilePage": () => MaterialPageRoute(
+          builder: (context) => Center(
+            child: Text("HomePage"),
+          ),
+        ),
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _children[_currentIndex],
+      body: _buildBody(),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: CustomColors.base_dark,
         selectedItemColor: CustomColors.base_white,
         unselectedItemColor: CustomColors.base_gray,
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
+        onTap: (routeIndex) => navigatorKey.currentState
+            .pushNamed(_pages.keys.toList()[routeIndex]),
         items: [
           BottomNavigationBarItem(
             //temporary names and icons
@@ -41,15 +62,14 @@ class _HomeState extends State<Home> {
             icon: new Icon(Icons.book_rounded),
             label: 'Dream Calendar',
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.person), label: 'Profile')
         ],
       ),
     );
   }
 
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
+  Widget _buildBody() => MaterialApp(
+      navigatorKey: navigatorKey,
+      onGenerateRoute: (route) => _pages[route.name]());
 }
--- lib/journalEdit.dart
+++ lib/journalEdit.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'calendar.dart';
-
-//https://www.appsdeveloperblog.com/buttons-with-icons-in-flutter/
-//https://flutter.dev/docs/cookbook/navigation/navigation-basics
+import 'customcolors.dart';
 
 class JournalEdit extends StatefulWidget {
   @override
@@ -13,11 +10,18 @@ class _JournalEditState extends State<JournalEdit> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-      children: [
-        Text("Hello"),
-      ],
-    ));
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Flexible(
+            child: FractionallySizedBox(
+              heightFactor: 0.04,
+            ),
+          ),
+          BackButton(color: CustomColors.base_white),
+        ],
+      ),
+    );
   }
 }
 
